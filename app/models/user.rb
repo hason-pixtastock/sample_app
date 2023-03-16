a8aea595d044d60cfa35f833a1236fc7ce597b90
hason-pixtastock <son.nguyenha@pixta.co.jp>
2023-03-16 13:48:43 +0700

    Finish the last chapter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     has_many :microposts, dependent: :destroy
+    has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+    has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
     attr_accessor :remember_token, :activation_token
     attr_accessor :remember_token, :activation_token, :reset_token
   
@@ -35,6 +39,13 @@ class User < ApplicationRecord
       update_attribute(:remember_digest, nil)
     end
 
+    def activate
+      update_columns(activated: true, activated_at: Time.now)
+    end
+
+    def send_activation_email
+        UserMailer.account_activation(self).deliver_now
+    end
 
     def create_activation_digest
       self.activation_token = User.new_token
@@ -56,10 +67,23 @@ class User < ApplicationRecord
     end
 
     def feed
-      Micropost.where("user_id = ?", id)
+      following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+      Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
     end
   
+    def follow(other_user)
+      active_relationships.create(followed_id: other_user.id)
+    end
 
+    # Unfollows a user.
+    def unfollow(other_user)
+      active_relationships.find_by(followed_id: other_user.id).destroy
+    end
+
+    # Returns true if the current user is following the other user.
+    def following?(other_user)
+      following.include?(other_user)
+    end
 
     private
     # Converts email to all lowercase.
