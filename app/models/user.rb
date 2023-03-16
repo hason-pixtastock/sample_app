2b8748e4b2a8f6620c3db3a2c4e680d12ed183ed
hason-pixtastock <126854183+hason-pixtastock@users.noreply.github.com>
2023-03-16 17:20:49 +0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,12 @@ class User < ApplicationRecord
 
     before_save { self.email = email.downcase }
     before_create :create_activation_digest
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :name, presence: true, length: {maximum:50}
-    validates :email, presence: true, length: {maximum:255}, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
+    validates :name, presence: true, length: { maximum: Settings.max_user_name_length }                
+    validates :email, presence: true, length: { maximum: Settings.max_user_email_length }, format: { with: Settings.valid_email_regex }, uniqueness: { case_sensitive: false }
     has_secure_password
-    validates :password, length: { minimum: 6 }, presence: true
+    validates :password, presence: true, length: { minimum: Settings.min_user_password_length}
+
+ 
     def User.digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
