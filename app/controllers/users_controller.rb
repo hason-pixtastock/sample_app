eb4510a7e29e0bdf2db5f228ef79f05b2e60bc2f
hason-pixtastock <son.nguyenha@pixta.co.jp>
2023-03-17 09:46:53 +0700

    Fix bug in chapter 13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,4 +29,47 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update 
+      @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+        flash[:success] = t(".update_user_noti")
+        redirect_to @user
+      else
+        render :edit
+      end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = t(".destroy_user_noti")
+    redirect_to users_url
+  end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+      store_location
+      flash[:danger] = t("log_in_required_noti")
+      redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
